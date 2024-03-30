dcf1d9e2ecc36b579aafb95d516314c9b2726d79
Sahil Sharma <sahil.sharma1912@gmail.com>
2024-03-30 18:52:49 +0530

    behaviour subjects

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {concat, interval, merge, of, Subject} from "rxjs";
+import {BehaviorSubject, concat, interval, merge, of, Subject} from "rxjs";
 import {map} from "rxjs/operators";
 import {createHttpObservable} from "../common/util";
 
@@ -15,16 +15,21 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-    const subject = new Subject();
+    const subject = new BehaviorSubject(0);
 
     const series$ = subject.asObservable();
 
-    series$.subscribe(console.log)
+    series$.subscribe(val => console.log('early sub: ', val));
 
     subject.next(1);
     subject.next(2);
     subject.next(3);
     subject.complete();
+
+
+    setTimeout(() => {
+      series$.subscribe(val => console.log('late sub: ', val));
+    }, 3000)
 
   }
 
