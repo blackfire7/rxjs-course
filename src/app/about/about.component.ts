1330665f58abfdaa74fe3c6fe24dc20d01639bab
Sahil Sharma <sahil.sharma1912@gmail.com>
2024-03-30 18:45:43 +0530

    subjects

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {concat, interval, merge, of} from "rxjs";
+import {concat, interval, merge, of, Subject} from "rxjs";
 import {map} from "rxjs/operators";
 import {createHttpObservable} from "../common/util";
 
@@ -15,11 +15,16 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-    // const http$ = createHttpObservable('/api/courses');
-    //
-    // const sub = http$.subscribe(console.log);
-    //
-    // setTimeout(() => sub.unsubscribe(), 0);
+    const subject = new Subject();
+
+    const series$ = subject.asObservable();
+
+    series$.subscribe(console.log)
+
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+    subject.complete();
 
   }
 
