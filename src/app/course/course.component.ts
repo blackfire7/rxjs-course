daea5a0d9f9ac1dde9972ab5e16da535eba66cfa
Sahil Sharma <sahil.sharma1912@gmail.com>
2024-03-30 15:47:19 +0530

    fork join

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,8 +1,8 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Course} from "../model/course";
-import {debounceTime, distinctUntilChanged, map, startWith, switchMap} from 'rxjs/operators';
-import {fromEvent, Observable} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map, startWith, switchMap, tap} from 'rxjs/operators';
+import {forkJoin, fromEvent, Observable} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import {createHttpObservable} from "../common/util";
 import {debug, RxJsLoggingLevel, setRxjsLoggingLevel} from "../common/debug";
@@ -32,11 +32,22 @@ export class CourseComponent implements OnInit, AfterViewInit {
   ngOnInit() {
 
     this.courseId = this.route.snapshot.params['id'];
-    this.course$ = createHttpObservable(`/api/courses/${this.courseId}/`).pipe(
-      debug(RxJsLoggingLevel.INFO, 'course value')
-    );
+    this.course$ = createHttpObservable(`/api/courses/${this.courseId}/`);
 
     setRxjsLoggingLevel(RxJsLoggingLevel.TRACE);
+
+    // FORK JOIN
+    // const course$ = createHttpObservable(`/api/courses/${this.courseId}/`);
+    // const lessons$ = this.loadLessons();
+    //
+    // forkJoin([course$, lessons$])
+    //   .pipe(
+    //     tap(([course, lessons]) => {
+    //       console.log('course', course)
+    //       console.log('lesson', lessons)
+    //     })
+    //   )
+    //   .subscribe(console.log)
 
   }
 
