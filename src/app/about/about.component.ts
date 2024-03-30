970f90c8e8a009bfe747c07d0ed38d0a51b6836c
Sahil Sharma <sahil.sharma1912@gmail.com>
2024-03-30 09:41:03 +0530

    observable creation for fetch() from scratch

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'about',
@@ -10,6 +11,26 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const http$ = new Observable(observer => {
+      observer.next(5);
+      observer.next(6);
+      observer.next(7);
+
+      fetch('/api/courses').then(response => {
+        return response.json();
+      }).then(body => {
+        observer.next(body);
+        observer.complete();
+      }).catch(error => {
+        observer.error(error);
+      });
+    });
+
+    http$.subscribe({
+      next: courses => console.log(courses),
+      error: error => console.log(error),
+      complete: () => console.log('Completed'),
+    });
   }
 
 }
