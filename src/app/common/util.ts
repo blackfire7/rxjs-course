e1ab8bfa135f4ce464bb74710467392c4dd145b6
Sahil Sharma <sahil.sharma1912@gmail.com>
2024-03-30 12:59:54 +0530

    debounceTime & distinctUntilChanged

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -2,14 +2,25 @@ import {Observable} from "rxjs";
 
 export function createHttpObservable(url: string) {
   return new Observable(observer => {
-    fetch(url).then(response => {
-      return response.json();
-    }).then(body => {
-      observer.next(body);
-      observer.complete();
-    }).catch(error => {
-      observer.error(error);
-    });
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+
+    fetch(url, {signal})
+      .then(response => {
+        return response.json();
+      })
+      .then(body => {
+        observer.next(body);
+        observer.complete();
+      })
+      .catch(error => {
+        observer.error(error);
+      });
+
+
+    return () => controller.abort();
   });
 }
 
